5fe29a8ba72d6a520582b2a0c448f7fb64765758
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-04-25 02:19:20 +0100

    Update SysTick.c

--- SysTick.c
+++ SysTick.c
@@ -1,4 +1,4 @@
-#include "stm32f4xx.h"                        // Device header
+#include "stm32f4xx.h"                                    // Device header
 
 void SystTickDelayMs( int n);
 void Setup(void);
@@ -15,18 +15,18 @@ int main(void)
 
 void Setup()
 {
-	RCC->AHB1ENR |=1;      											// Enabling AHB1 Clock
-	GPIOA->MODER |=0x20;   											// Setting PA5 as output 
+	RCC->AHB1ENR |=1;      				  // Enabling AHB1 Clock
+	GPIOA->MODER |=0x20;   				  // Setting PA5 as output 
 }
 
 void SystTickDelayMs( int n)
 {
-	SysTick->LOAD =16000; 											// Reload with number of clocks per millisecond
-	SysTick->VAL  =0;     								  		// Clear Value register
-  SysTick->CTRL = 0x5; 										    // Enable SysTick
+	SysTick->LOAD =16000; 		               	// Reload with number of clocks per millisecond
+	SysTick->VAL  =0;     				// Clear Value register
+  SysTick->CTRL = 0x5; 				        // Enable SysTick
 	for(int i=0;i<n;i++)
 	{
-  while((SysTick->CTRL & 0x10000) ==0){} 	    // Wait until the COUNT flag is set
+  while((SysTick->CTRL & 0x10000) ==0){} 	        // Wait until the COUNT flag is set
 	}
 	SysTick->CTRL=0;
 }
