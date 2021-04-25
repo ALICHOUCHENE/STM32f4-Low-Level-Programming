d2490896b7684816a254c05330f877bd2d17dd06
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-04-25 17:47:00 +0100

    Update Output Compare Mode.c

--- Output Compare Mode.c
+++ Output Compare Mode.c
@@ -1,8 +1,11 @@
-#include "stm32f4xx.h"                  // Device header.
+#include "stm32f4xx.h"                 				  // Device header
 
 // Toggle onboard LED connected to PA5 using TIM2 with output compare mode.
+
 void Toggle_Output_Compare_Mode(void);
+
 void Setup(void);
+
 int main(void)
 {
 	Setup();
@@ -10,18 +13,18 @@ int main(void)
 }
 void Setup(void)
 {
-	RCC->AHB1ENR |=1; // Enabling AHB1 Clock.
-	GPIOA->MODER |= 0x800; // Set PA5 to Alternate Function.
-	GPIOA->AFR[0] |=0x100000; // Set pin Af1 for TIM2 CHANNEL_1.
+	RCC->AHB1ENR |=1; 					// Enabling AHB1 Clock
+	GPIOA->MODER |= 0x800;				        // Set PA5 to Alternate Function
+	GPIOA->AFR[0] |=0x100000; 				// Set pin Af1 for TIM2 CHANNEL_1
 	RCC->APB1ENR |=1;
 }
 void Toggle_Output_Compare_Mode(void)
 {
 	TIM2->PSC =1600-1;
 	TIM2->ARR =100000-1;
-	TIM2->CCMR1 =0x30; // Set output to toggle on match.
-	TIM2->CCR1 =0;     // St match mode.
-	TIM2->CCER |=1; // Enable CH1 compare mode.
-	TIM2->CNT =0; // Clear counter.
-	TIM2->CR1 |=1; // Enable TIM2.
+	TIM2->CCMR1 =0x30; 					// Set output to toggle on match
+	TIM2->CCR1 =0;    				        // St match mode
+	TIM2->CCER |=1; 					// Enable CH1 compare mode
+	TIM2->CNT =0;					        // Clear counter
+	TIM2->CR1 |=1; 						// Enable TIM2
 }
